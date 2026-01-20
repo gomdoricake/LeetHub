c0eaec5d1ad8d4894af0ac191ac2f036da81973c
junn <88719152+gomdoricake@users.noreply.github.com>
2026-01-20 22:47:00 +0900

    Time: 6968 ms (5.1%), Space: 30.5 MB (8.88%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,32 @@
+import copy
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        maxWater = [0 for i in range(n)]
+        
+        # height = [1,8,6,2,5,4,8,3,7]
+        # rank_idx = [0,3,7,5,4,2,8,1,6]
+        for i in range(n):
+
+            # from left
+            pointer = 0
+            while True: 
+                if pointer >= i:
+                    break
+                if height[pointer] >= height[i]:
+                    maxWater[i] = max(maxWater[i], (i - pointer) * height[i])
+                    break
+                pointer += 1
+
+            # from right
+            pointer = n-1
+            while True: 
+                if pointer <= i: 
+                    break
+                if height[pointer] >= height[i]: 
+                    maxWater[i] = max(maxWater[i], (pointer - i) * height[i])
+                    break   
+                pointer -= 1
+
+        return max(maxWater)
